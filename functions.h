3b616c5073aedd43eeb8fe64a6147bfd70e99651
Vermikc <gmaran822@gmail.com>
2021-06-30 10:51:57 +0300

    Added functions module

---
+++ functions.h
@@ -0,0 +1,10 @@
+#ifndef ANAGRAM_FUNCTIONS_H
+#define ANAGRAM_FUNCTIONS_H
+
+int MixWord(char* word, int len);
+char* CreateUsedCharsVector(int len);
+int ToLower(char* word, int len);
+int MixText(char* text, int len);
+int WriteWord(char* dest, char* word, int index_from, int length);
+
+#endif
