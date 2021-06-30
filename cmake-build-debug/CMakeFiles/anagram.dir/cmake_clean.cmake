d58b446262ab48e64dec46f78030eb0e5c065194
Vermikc <gmaran822@gmail.com>
2021-06-30 11:32:24 +0300

    Add cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/anagram.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anagram.dir/functions.c.obj"
+  "CMakeFiles/anagram.dir/main.c.obj"
+  "anagram.exe"
+  "anagram.exe.manifest"
+  "anagram.pdb"
+  "libanagram.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/anagram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
